8c09e73a40236f109a0dc59862276df2a233a6c4
mao <permanenced@gmail.com>
2026-05-28 15:37:35 +0800

    同步 data.js：30条作品，合并系列，更新年份

--- data.js
+++ data.js
@@ -15,12 +15,12 @@ window.portfolioData = [
         ]
     },
     {
-        "id": 1768055517544,
-        "title": "漓江风光",
-        "category": "风光",
-        "year": "2023",
+        "id": 1779949102759,
+        "title": "郴州必胜",
+        "category": "人文",
+        "year": "2026",
         "desc": "",
-        "cover": "images/1768055517524_《漓江风光2》.jpg",
+        "cover": "images/1779949102759_《郴州必胜》.jpg",
         "series": []
     },
     {
@@ -33,193 +33,17 @@ window.portfolioData = [
         "series": []
     },
     {
-        "id": 1779949102453,
-        "title": "北湖公园",
-        "category": "风光",
-        "year": "2017",
-        "desc": "",
-        "cover": "images/1779949102453_《北湖公园》.jpg",
-        "series": []
-    },
-    {
-        "id": 1779949102465,
-        "title": "回龙山",
-        "category": "风光",
-        "year": "2024",
-        "desc": "",
-        "cover": "images/1779949102465_《回龙山》.jpg",
-        "series": []
-    },
-    {
-        "id": 1779949102472,
-        "title": "回龙山星空",
-        "category": "风光",
-        "year": "2017",
-        "desc": "",
-        "cover": "images/1779949102472_《回龙山星空》.jpg",
-        "series": []
-    },
-    {
-        "id": 1779949102482,
-        "title": "回龙山星轨",
-        "category": "风光",
-        "year": "2024",
-        "desc": "",
-        "cover": "images/1779949102482_《回龙山星轨》.jpg",
-        "series": []
-    },
-    {
-        "id": 1779949102498,
-        "title": "夜",
-        "category": "人文",
-        "year": "2017",
-        "desc": "",
-        "cover": "images/1779949102498_《夜》.jpg",
-        "series": []
-    },
-    {
-        "id": 1779949102509,
-        "title": "大眼睛拍于黑坦",
-        "category": "人文",
-        "year": "2022",
-        "desc": "",
-        "cover": "images/1779949102509_《大眼睛》拍于黑坦.jpg",
-        "series": []
-    },
-    {
-        "id": 1779949102530,
-        "title": "孤独",
-        "category": "人文",
-        "year": "2017",
-        "desc": "",
-        "cover": "images/1779949102530_《孤独》.jpg",
-        "series": []
-    },
-    {
-        "id": 1779949102539,
-        "title": "宁静",
-        "category": "人文",
-        "year": "2017",
-        "desc": "",
-        "cover": "images/1779949102539_《宁静》.jpg",
-        "series": []
-    },
-    {
         "id": 1779949102545,
-        "title": "山石矶风光1",
+        "title": "山石矶风光",
         "category": "风光",
         "year": "2025",
         "desc": "",
         "cover": "images/1779949102545_《山石矶风光1》.jpg",
-        "series": []
-    },
-    {
-        "id": 1779949102555,
-        "title": "山石矶风光2",
-        "category": "风光",
-        "year": "2025",
-        "desc": "",
-        "cover": "images/1779949102555_《山石矶风光2》.jpg",
-        "series": []
-    },
-    {
-        "id": 1779949102565,
-        "title": "山石矶风光3",
-        "category": "风光",
-        "year": "2025",
-        "desc": "",
-        "cover": "images/1779949102565_《山石矶风光3》.jpg",
-        "series": []
-    },
-    {
-        "id": 1779949102575,
-        "title": "山石矶风光4",
-        "category": "风光",
-        "year": "2025",
-        "desc": "",
-        "cover": "images/1779949102575_《山石矶风光4》.jpg",
-        "series": []
-    },
-    {
-        "id": 1779949102585,
-        "title": "日出",
-        "category": "风光",
-        "year": "2017",
-        "desc": "",
-        "cover": "images/1779949102585_《日出》.jpg",
-        "series": []
-    },
-    {
-        "id": 1779949102598,
-        "title": "日出回龙山",
-        "category": "风光",
-        "year": "2016",
-        "desc": "",
-        "cover": "images/1779949102598_《日出回龙山》.jpg",
-        "series": []
-    },
-    {
-        "id": 1779949102605,
-        "title": "晨捕",
-        "category": "人文",
-        "year": "2016",
-        "desc": "",
-        "cover": "images/1779949102605_《晨捕》.jpg",
-        "series": []
-    },
-    {
-        "id": 1779949102613,
-        "title": "洞穴人家",
-        "category": "人文",
-        "year": "2023",
-        "desc": "",
-        "cover": "images/1779949102613_《洞穴人家》.jpg",
-        "series": []
-    },
-    {
-        "id": 1779949102623,
-        "title": "灯绘-1",
-        "category": "风光",
-        "year": "2026",
-        "desc": "",
-        "cover": "images/1779949102623_《灯绘》-1.jpg",
-        "series": []
-    },
-    {
-        "id": 1779949102628,
-        "title": "灯绘-2",
-        "category": "风光",
-        "year": "2026",
-        "desc": "",
-        "cover": "images/1779949102628_《灯绘》-2.jpg",
-        "series": []
-    },
-    {
-        "id": 1779949102640,
-        "title": "灯绘-3",
-        "category": "风光",
-        "year": "2026",
-        "desc": "",
-        "cover": "images/1779949102640_《灯绘》-3.jpg",
-        "series": []
-    },
-    {
-        "id": 1779949102650,
-        "title": "灯绘-4",
-        "category": "风光",
-        "year": "2026",
-        "desc": "",
-        "cover": "images/1779949102650_《灯绘》-4.jpg",
-        "series": []
-    },
-    {
-        "id": 1779949102657,
-        "title": "灯绘-5",
-        "category": "风光",
-        "year": "2026",
-        "desc": "",
-        "cover": "images/1779949102657_《灯绘》-5.jpg",
-        "series": []
+        "series": [
+            "images/1779949102555_《山石矶风光2》.jpg",
+            "images/1779949102565_《山石矶风光3》.jpg",
+            "images/1779949102575_《山石矶风光4》.jpg"
+        ]
     },
     {
         "id": 1779949102664,
@@ -231,12 +55,12 @@ window.portfolioData = [
         "series": []
     },
     {
-        "id": 1779949102673,
-        "title": "盐洲岛",
+        "id": 1779949102465,
+        "title": "回龙山",
         "category": "风光",
-        "year": "2017",
+        "year": "2024",
         "desc": "",
-        "cover": "images/1779949102673_《盐洲岛》.jpg",
+        "cover": "images/1779949102465_《回龙山》.jpg",
         "series": []
     },
     {
@@ -258,69 +82,6 @@ window.portfolioData = [
         "series": []
     },
     {
-        "id": 1779949102704,
-        "title": "穿过一线天",
-        "category": "人文",
-        "year": "2023",
-        "desc": "",
-        "cover": "images/1779949102704_《穿过一线天》.jpg",
-        "series": []
-    },
-    {
-        "id": 1779949102713,
-        "title": "紫鹊界",
-        "category": "风光",
-        "year": "2017",
-        "desc": "",
-        "cover": "images/1779949102713_《紫鹊界》.jpg",
-        "series": []
-    },
-    {
-        "id": 1779949102725,
-        "title": "莽山风光1",
-        "category": "风光",
-        "year": "2022",
-        "desc": "",
-        "cover": "images/1779949102725_《莽山风光1》.jpg",
-        "series": []
-    },
-    {
-        "id": 1779949102735,
-        "title": "莽山风光2",
-        "category": "风光",
-        "year": "2022",
-        "desc": "",
-        "cover": "images/1779949102735_《莽山风光2》.jpg",
-        "series": []
-    },
-    {
-        "id": 1779949102742,
-        "title": "莽山风光3",
-        "category": "风光",
-        "year": "2023",
-        "desc": "",
-        "cover": "images/1779949102742_《莽山风光3》.jpg",
-        "series": []
-    },
-    {
-        "id": 1779949102752,
-        "title": "莽山风光4",
-        "category": "风光",
-        "year": "2024",
-        "desc": "",
-        "cover": "images/1779949102752_《莽山风光4》.jpg",
-        "series": []
-    },
-    {
-        "id": 1779949102759,
-        "title": "郴州必胜",
-        "category": "人文",
-        "year": "2014",
-        "desc": "",
-        "cover": "images/1779949102759_《郴州必胜》.jpg",
-        "series": []
-    },
-    {
         "id": 1779949102768,
         "title": "金仙寨星轨",
         "category": "风光",
@@ -339,21 +100,30 @@ window.portfolioData = [
         "series": []
     },
     {
-        "id": 1779949102786,
-        "title": "铁鼎寨",
+        "id": 1768055517544,
+        "title": "漓江风光",
         "category": "风光",
-        "year": "2022",
+        "year": "2023",
         "desc": "",
-        "cover": "images/1779949102786_《铁鼎寨》.jpg",
+        "cover": "images/1768055517524_《漓江风光2》.jpg",
         "series": []
     },
     {
-        "id": 1779949102795,
-        "title": "雾漫小东江",
-        "category": "风光",
-        "year": "2016",
+        "id": 1779949102613,
+        "title": "洞穴人家",
+        "category": "人文",
+        "year": "2023",
         "desc": "",
-        "cover": "images/1779949102795_《雾漫小东江》.jpg",
+        "cover": "images/1779949102613_《洞穴人家》.jpg",
+        "series": []
+    },
+    {
+        "id": 1779949102704,
+        "title": "穿过一线天",
+        "category": "人文",
+        "year": "2023",
+        "desc": "",
+        "cover": "images/1779949102704_《穿过一线天》.jpg",
         "series": []
     },
     {
@@ -366,39 +136,153 @@ window.portfolioData = [
         "series": []
     },
     {
-        "id": 1779949102811,
-        "title": "高跟鞋",
-        "category": "人文",
-        "year": "2025",
-        "desc": "",
-        "cover": "images/1779949102811_《高跟鞋》.jpg",
-        "series": []
-    },
-    {
         "id": 1779949102821,
-        "title": "龙脊梯田1",
+        "title": "龙脊梯田",
         "category": "风光",
         "year": "2023",
         "desc": "",
         "cover": "images/1779949102821_《龙脊梯田1》.jpg",
-        "series": []
+        "series": [
+            "images/1779949102833_《龙脊梯田2》 拷贝.jpg"
+        ]
     },
     {
-        "id": 1779949102833,
-        "title": "龙脊梯田2",
+        "id": 1779949102482,
+        "title": "回龙山星轨",
         "category": "风光",
-        "year": "2023",
+        "year": "2022",
         "desc": "",
-        "cover": "images/1779949102833_《龙脊梯田2》 拷贝.jpg",
+        "cover": "images/1779949102482_《回龙山星轨》.jpg",
         "series": []
     },
     {
-        "id": 1779949102844,
-        "title": "谢斌",
-        "category": "人像",
-        "year": "2014",
+        "id": 1779949102509,
+        "title": "大眼睛拍于黑坦",
+        "category": "人文",
+        "year": "2022",
         "desc": "",
-        "cover": "images/1779949102844_谢斌.JPG",
+        "cover": "images/1779949102509_《大眼睛》拍于黑坦.jpg",
+        "series": []
+    },
+    {
+        "id": 1779949102725,
+        "title": "莽山风光",
+        "category": "风光",
+        "year": "2022",
+        "desc": "",
+        "cover": "images/1779949102725_《莽山风光1》.jpg",
+        "series": [
+            "images/1779949102735_《莽山风光2》.jpg",
+            "images/1779949102742_《莽山风光3》.jpg",
+            "images/1779949102752_《莽山风光4》.jpg"
+        ]
+    },
+    {
+        "id": 1779949102786,
+        "title": "铁鼎寨",
+        "category": "风光",
+        "year": "2022",
+        "desc": "",
+        "cover": "images/1779949102786_《铁鼎寨》.jpg",
+        "series": []
+    },
+    {
+        "id": 1779949102453,
+        "title": "北湖公园",
+        "category": "风光",
+        "year": "2017",
+        "desc": "",
+        "cover": "images/1779949102453_《北湖公园》.jpg",
+        "series": []
+    },
+    {
+        "id": 1779949102472,
+        "title": "回龙山星空",
+        "category": "风光",
+        "year": "2017",
+        "desc": "",
+        "cover": "images/1779949102472_《回龙山星空》.jpg",
+        "series": []
+    },
+    {
+        "id": 1779949102498,
+        "title": "夜",
+        "category": "人文",
+        "year": "2017",
+        "desc": "",
+        "cover": "images/1779949102498_《夜》.jpg",
+        "series": []
+    },
+    {
+        "id": 1779949102530,
+        "title": "孤独",
+        "category": "人文",
+        "year": "2017",
+        "desc": "",
+        "cover": "images/1779949102530_《孤独》.jpg",
+        "series": []
+    },
+    {
+        "id": 1779949102539,
+        "title": "宁静",
+        "category": "人文",
+        "year": "2017",
+        "desc": "",
+        "cover": "images/1779949102539_《宁静》.jpg",
+        "series": []
+    },
+    {
+        "id": 1779949102585,
+        "title": "日出",
+        "category": "风光",
+        "year": "2017",
+        "desc": "",
+        "cover": "images/1779949102585_《日出》.jpg",
+        "series": []
+    },
+    {
+        "id": 1779949102673,
+        "title": "盐洲岛",
+        "category": "风光",
+        "year": "2017",
+        "desc": "",
+        "cover": "images/1779949102673_《盐洲岛》.jpg",
+        "series": []
+    },
+    {
+        "id": 1779949102713,
+        "title": "紫鹊界",
+        "category": "风光",
+        "year": "2017",
+        "desc": "",
+        "cover": "images/1779949102713_《紫鹊界》.jpg",
+        "series": []
+    },
+    {
+        "id": 1779949102598,
+        "title": "日出回龙山",
+        "category": "风光",
+        "year": "2016",
+        "desc": "",
+        "cover": "images/1779949102598_《日出回龙山》.jpg",
+        "series": []
+    },
+    {
+        "id": 1779949102605,
+        "title": "晨捕",
+        "category": "人文",
+        "year": "2016",
+        "desc": "",
+        "cover": "images/1779949102605_《晨捕》.jpg",
+        "series": []
+    },
+    {
+        "id": 1779949102795,
+        "title": "雾漫小东江",
+        "category": "风光",
+        "year": "2016",
+        "desc": "",
+        "cover": "images/1779949102795_《雾漫小东江》.jpg",
         "series": []
     }
 ];
